35bceb59db713c2cb276872a85534e9895c2ad2a
Aaron Leon <62915096+aaronl99l@users.noreply.github.com>
2022-02-23 05:34:16 -0500

    Adding updated back-end UI

---
+++ login_script.js
@@ -0,0 +1,14 @@
+var btn = document.getElementById("login_btn");
+
+btn.addEventListener("click", function(){
+	var name = document.getElementById("name").value;
+	var pass = document.getElementById("pass").value;
+	if(name === "Marcos" && pass === "xyz123")
+	{
+		location.href = "manager_page.html";
+	}
+	if(name === "Aaron" && pass === "abc789")
+	{
+		location.href = "employee_page.html";
+	}
+});
